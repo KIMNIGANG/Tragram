2f953c30df85ca10e1f80f98633cf7d3580c95e2
kimnigang <zeus2424797@gmail.com>
2022-05-25 14:55:57 +0900

    auth_pass code

--- app/controllers/instagram_auth_controller.rb
+++ app/controllers/instagram_auth_controller.rb
@@ -4,15 +4,15 @@ class InstagramAuthController < ApplicationController
     def index
     end
 
-    # instagram認証ボタン
-    def auth
-
-    end
-
     # リダイレクト先
     # もらったcodeをpush
+    
     def auth_pass
       code = params[:code]
+      uri = URI('https://api.instagram.com/oauth/access_token')
+      uri.query = URI.encode_www_form({
+      client_id: '555005406003631', client_secret: '3a993bf3425dda372bbb5cd264b39bf4', grant_type: 'authorization_code' , redirect_uri: 'https://github.com/KIMNIGANG/Tragram', code: code
+      })
 
     end
 
@@ -21,7 +21,4 @@ class InstagramAuthController < ApplicationController
       token = params[:access_token]
       user_id = params[:user_id]
     end
-
-    def 
-
-    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'instagram_auth#index'
+  get '/authpass' => 'instagram_auth#auth_pass'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
