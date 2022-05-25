d2eb7e002b2ea150b78bf67650dc3aae78408c75
takato <takato1043233@gmail.com>
2022-05-25 14:03:50 +0900

    4methods

--- app/controllers/instagram_auth_controller.rb
+++ app/controllers/instagram_auth_controller.rb
@@ -1,4 +1,27 @@
+require 'uri'
+
 class InstagramAuthController < ApplicationController
     def index
     end
-end
+
+    # instagram認証ボタンを押すと発動
+    def auth
+
+    end
+
+    # instagramに指定するリダイレクト先
+    # 4段階 codeをparamsから取って、再びpush
+    def auth_pass
+      code = params[:code]
+
+    end
+
+    # トークン受け取り
+    def auth_get_token
+      token = params[:access_token]
+      user_id = params[:user_id]
+    end
+
+    def 
+
+    end
