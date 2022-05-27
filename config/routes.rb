64bd6129c05038631f5399ca5c970a7ef5e17ac2
pecokerochan <lisahirano@13351112090.ap.cc.tsukuba.ac.jp>
2022-05-27 17:42:08 +0900

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #あるユーザーのmypage表示
   get '/:user_id/mypage', to:'users#mypage'
+
+  #post追加
+  get '/:project_id/add_post', to:'projects#add_post'
+
+  #あるprojectのpost表示
+  get '/:project_id', to: 'projects#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
