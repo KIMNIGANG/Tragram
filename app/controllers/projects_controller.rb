37b9785f0e16ac173a0a92380c5ea9e9e7a8b672
takato <takato1043233@gmail.com>
2022-06-01 14:49:31 +0900

    show(controller), model's relation

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,12 @@
+class ProjectsController < ApplicationController
+
+  def show()
+    @project = Project.find_by(id: params[:project_id])
+    if (@project)
+      @posts = @project.posts.order()
+    end
+  end
+
+
+
+
---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+class Project < ApplicationRecord
+  has_many: posts
+  has_many: users
+
+
+
+
+end
