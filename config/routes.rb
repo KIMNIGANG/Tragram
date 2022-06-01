468c4b48f4571336488e86d104d00a61577b7c4e
pecokerochan <lisahirano@13351113230.ap.cc.tsukuba.ac.jp>
2022-06-01 14:35:06 +0900

    user_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   #あるユーザーのmypage表示
-  get '/:user_id/mypage', to:'users#mypage'
+  get '/:user_id/show', to:'users#show'
 
   #post追加
   get '/:project_id/add_post', to:'projects#add_post'
 
-  #あるprojectのpost表示
-  get '/:project_id', to: 'projects#show'
+  #あるprojectのページを表示
+  get '/project/:project_id', to: 'projects#show'
+
+  resources :users, only:[:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
