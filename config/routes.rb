ce85896df925eac613a4ec619248ff26e220e1d8
pecokerochan <lisahirano@13351113019.ap.cc.tsukuba.ac.jp>
2022-06-03 17:19:01 +0900

    mainとの差分解消

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,5 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[create destroy]
   get '/' => 'instagram_auth#index'
   get '/authpass', to: 'instagram_auth#auth_pass'
-  #あるユーザーのmypage表示
-  get '/:user_id/show', to:'users#show'
-
-  #post追加
-  get '/:project_id/add_post', to:'projects#add_post'
-
-  #あるprojectのpost表示
-  get '/:project_id', to: 'projects#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
