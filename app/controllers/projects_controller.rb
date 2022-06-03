b8cb542968b8d8d60eeed1c779d1d521b9727bdc
takato <takato1043233@gmail.com>
2022-06-03 16:36:53 +0900

    method * 4

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,55 @@
-class ProjectsController < ApplicationController
+class ProjectsController < ApplicationControllera
+  require 'rack'
+
+  before_action(only: %i[show destroy edit]){project = Project.find_by(id: params[:project_id])}
+
+  def create()
+    # query = ?members=[id=hoge,id=foo,id=bar]
+    members = params[:members]
+    Rack::Utils.parse_nested_query(members)
+    members.each do |member|
+      id = member.
+
+  end
 
   def show()
-    @project = Project.find_by(id: params[:project_id])
-    if (@project)
-      @posts = @project.posts.order()
+    if project && current_user.id in project.members.id
+      @project = project
+    else
+      redirect_to request.referer, notice: 'you have no access'
     end
+
+
+  def destroy()
+    # need to define "current_user" method
+    # "members" defined in project model
+    if project && current_user.id in project.members.id
+      project.destroy
+      flash[:success] = 'deleted project'
+    else
+      flash[:danger] = 'failed to delete project'
+    end
+
+    # redirect to "home"
+    # use command "$ rake routes" to see prefix
+    redirect_to :root_path
   end
+
+  def edit_metadata()
+    # need to define "current_user"
+    # "members" defined in project model
+    if project && current_user.id in project.members.id
+      @name = project.name
+      @caption = project.caption
+    end
+
+    # redirect to referer
+    redirect_to request.referer
+  end
+
+  
+
+
 
   
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,3 +4,4 @@ class Project < ApplicationRecord
 
 
 end
+
