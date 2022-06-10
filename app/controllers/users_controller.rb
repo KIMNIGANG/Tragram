d6399661d13dfb5d570a18866a77e3d82ac76fcc
pecokerochan <lisahirano@13351112109.ap.cc.tsukuba.ac.jp>
2022-06-10 17:43:50 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  has_many :projects, through: :user_project
+
+  def show
+    #@projects = current_userの所属してるprojects全部
+    @projects = users.projects
+  end
 end
