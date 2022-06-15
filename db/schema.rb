cb81e57e377fd522ce5be3a64c44d0dff4ec9ebb
pecokerochan <lisahirano@13351113239.ap.cc.tsukuba.ac.jp>
2022-06-15 14:10:51 +0900

    rename table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_084521) do
+ActiveRecord::Schema.define(version: 2022_06_10_063913) do
 
   create_table "images", force: :cascade do |t|
     t.string "url"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_06_10_084521) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "user_project", force: :cascade do |t|
+  create_table "user_projects", force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
