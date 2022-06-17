a5d62cb9a5fdfc060b9cf88784040fcbb242b8be
pecokerochan <lisahirano@MacBook-Pro.local>
2022-06-17 21:05:18 +0900

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,41 +6,14 @@ class PostsController < ApplicationController
 
   def create
     #formからcaptionを受け取ってレコード作成
-    if current_user
-        current_user.posts.create(caption:params[:caption])
-        redirect_to request.referer
-    end
+    post = Post.create(post_params)
+    post.save
+    redirect_to root_path
   end
-
-  def destroy
-	  if (post = Post.find(params[:id])) && (current_user.id == projectに所属してる人のユーザーid)
-      post.destroy
-      flash[:success] = '削除しました'
-    else
-      flash[:danger] = '削除に失敗しました'
-    end
-    redirect_to request.referer
-  end
-
-  def edit
-    #編集情報を渡す
-    @post =  Post.find(params[:id])
-  end
-
-  def update
-    #情報を更新
-    @post = Post.find(params[:id])
-    @post = Post.update(post_params)
-    redirect_to posts.referer #リダイレクト先どこがいい?
-  end
-
-
-  private
-
 
   def post_params
     #悪意あるユーザからの情報を受け取らないように
-    params.require(:post).permit(:caption,:project_id,:id) #?????
+    params.require(:post).permit(:caption)
   end
 
 end
