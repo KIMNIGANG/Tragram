ddb4ad624df5dd3da0f714c7835b5ab34693f5a5
pecokerochan <lisahirano@13351113144.ap.cc.tsukuba.ac.jp>
2022-07-06 14:23:38 +0900

    invitation complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  #before_action :store_location
   before_action :check_logged_in
 
   def check_logged_in
@@ -8,12 +7,5 @@ class ApplicationController < ActionController::Base
     return if '/projects/:id/invite'
     redirect_to root_path
   end
-
-  #def store_location
-    # store last url as long as it isn't a /users path
-    #if (request.fullpath != "/")
-    #  session[:previous_url] = request.referer
-    #end
-  #end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,15 @@
 class SessionsController < ApplicationController
   skip_before_action :check_logged_in, only: :create
-  before_action :store_location, only: :create
 
   def create
     if (user = User.find_or_create_from_auth_hash(auth_hash))
       log_in user
     end
-    #if session[:previous_url].present?
-    #    print "ここからセッション"
-    #    print session[:previous_url]
-    #    redirect_to "projects/:id/invite"
-    #else
+    if session[:invite_url].present?
+        redirect_to session[:invite_url]
+    else
        redirect_to root_path
-    #end
+    end
   end
 
   def destroy
