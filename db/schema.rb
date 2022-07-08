e57367e6dea707d61586be1e9b0c8f7bd5d9a2ce
Jikkensan <jikken@13351119073.ap.cc.tsukuba.ac.jp>
2022-07-08 16:49:22 +0900

    軽微な修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_070327) do
+ActiveRecord::Schema.define(version: 2022_06_22_144348) do
 
   create_table "images", force: :cascade do |t|
     t.string "url"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2022_07_08_070327) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "project_id"
-    t.string "image"
   end
 
   create_table "projects", force: :cascade do |t|
