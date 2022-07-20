36b9034bfad554666240574cad2aa1af4c1cdaae
kimnigang <zeus2424797@gmail.com>
2022-07-20 13:15:52 +0900

    c

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -11,20 +11,20 @@ function initAutocomplete() {
     mapTypeId: "roadmap",
   });
 
-  document.getElementById("set-pin").addEventListener("click", () => {
-    place_geo.forEach(function (t) {
-      console.log(t.lat);
-      markers.push(
-        new google.maps.Marker({
-          map: map,
-          icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-          title: t.name,
-          position: { lat: t.lat, lng: t.lng },
-          animation: google.maps.Animation.DROP,
-        })
-      );
-    });
-  });
+  // document.getElementById("set-pin").addEventListener("click", () => {
+  //   place_geo.forEach(function (t) {
+  //     console.log(t.lat);
+  //     markers.push(
+  //       new google.maps.Marker({
+  //         map: map,
+  //         icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+  //         title: t.name,
+  //         position: { lat: t.lat, lng: t.lng },
+  //         animation: google.maps.Animation.DROP,
+  //       })
+  //     );
+  //   });
+  // });
 
   const input = document.getElementById("pac-input");
   const searchBox = new google.maps.places.SearchBox(input);
@@ -37,12 +37,12 @@ function initAutocomplete() {
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener("places_changed", function () {
-    document.getElementById("clear-pin").addEventListener("click", () => {
-      // Clear out the old markers.
-      markers.forEach(function (marker) {
-        marker.setMap(null);
-      });
-    });
+    // document.getElementById("clear-pin").addEventListener("click", () => {
+    //   // Clear out the old markers.
+    //   markers.forEach(function (marker) {
+    //     marker.setMap(null);
+    //   });
+    // });
     var places = searchBox.getPlaces();
     if (places.length == 0) {
       return;
@@ -95,8 +95,12 @@ function initAutocomplete() {
       const place_list = document.getElementById("place-list");
       let st = document.createElement("div");
       st.innerHTML = `
-        <input type="hidden" name="lat" value="${place.geometry["location"].lat}">
-        <input type="hidden" name="lng" value="${place.geometry["location"].lng}">
+        <input type="hidden" name="lat" value="${place.geometry[
+          "location"
+        ].lat()}">
+        <input type="hidden" name="lng" value="${place.geometry[
+          "location"
+        ].lng()}">
         <input type="hidden" name="name" value="${place.name}">
         `;
       place_list.appendChild(st);
