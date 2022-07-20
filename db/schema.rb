e07e8bcf89952ce027e1661bed813743a2aaaa44
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-20 12:50:46 +0900

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2022_07_08_083138) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.float "lat"
+    t.float "lng"
+    t.string "name"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "post_images", force: :cascade do |t|
     t.integer "image_id"
     t.integer "post_id"
