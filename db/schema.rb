ba5aaa73d791fc0a54c05dc2dc99527be7b208df
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-20 12:52:07 +0900

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_07_08_083138) do
 
 
 
+
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.string "media_type"
@@ -54,6 +55,8 @@ ActiveRecord::Schema.define(version: 2022_07_08_083138) do
     t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "project_id"
+    t.string "image"
   end
 
   create_table "projects", force: :cascade do |t|
