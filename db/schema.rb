22e10597d8bb707c4381f4ab98b3446d8004aa40
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-20 12:50:09 +0900

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
+
 ActiveRecord::Schema.define(version: 2022_07_08_083138) do
+
 
 
   create_table "images", force: :cascade do |t|
