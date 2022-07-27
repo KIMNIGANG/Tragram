2f51ecb3413a7ab3d16fa4014b75ff2d41c93767
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-28 00:35:50 +0900

    rebase

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,7 +2,6 @@
 
 Object.defineProperty(exports, "__esModule", { value: true });
 
-<<<<<<< HEAD
 const place_geo = [];
 
 function initAutocomplete() {
@@ -28,23 +27,11 @@ function initAutocomplete() {
 
   const input = document.getElementById("pac-input");
   const searchBox = new google.maps.places.SearchBox(input);
-=======
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 36.0847492, lng: 140.1037952 },
-    zoom: 14,
-    mapTypeId: "roadmap",
-  });
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById("pac-input");
-  var searchBox = new google.maps.places.SearchBox(input);
->>>>>>> 8cf2ca3 (conflict)
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   // Bias the SearchBox results towards current map's viewport.
   map.addListener("bounds_changed", function () {
     searchBox.setBounds(map.getBounds());
   });
-<<<<<<< HEAD
   let markers = [];
   markers.push(
     new google.maps.Marker({
@@ -57,23 +44,11 @@ function initAutocomplete() {
       animation: google.maps.Animation.DROP,
     })
   );
-=======
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
->>>>>>> 8cf2ca3 (conflict)
   searchBox.addListener("places_changed", function () {
     var places = searchBox.getPlaces();
     if (places.length == 0) {
       return;
     }
-<<<<<<< HEAD
-=======
-    // Clear out the old markers.
-    markers.forEach(function (marker) {
-      marker.setMap(null);
-    });
->>>>>>> 8cf2ca3 (conflict)
     markers = [];
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
@@ -83,30 +58,19 @@ function initAutocomplete() {
         return;
       }
       var icon = {
-<<<<<<< HEAD
-=======
-        url: place.icon,
->>>>>>> 8cf2ca3 (conflict)
         size: new google.maps.Size(71, 71),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25),
       };
-<<<<<<< HEAD
       // ピン　立てる
-=======
-      // Create a marker for each place.
->>>>>>> 8cf2ca3 (conflict)
       markers.push(
         new google.maps.Marker({
           map: map,
           icon: icon,
           title: place.name,
           position: place.geometry.location,
-<<<<<<< HEAD
           animation: google.maps.Animation.DROP,
-=======
->>>>>>> 8cf2ca3 (conflict)
         })
       );
       if (place.geometry.viewport) {
