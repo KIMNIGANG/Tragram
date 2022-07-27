d37abc4f81eaf2cb6034dfa7767a86e1b9ded9af
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-28 00:33:16 +0900

    rebase

--- app/controllers/google_map_controller.rb
+++ app/controllers/google_map_controller.rb
@@ -2,6 +2,7 @@ class GoogleMapController < ApplicationController
   require 'net/http'
   require 'uri'
   def index
+<<<<<<< HEAD
     require "uri"
     require "net/http"
 
@@ -21,4 +22,38 @@ class GoogleMapController < ApplicationController
   def map
 
   end
+=======
+    #userのメディアidを取ってくる (写真のid)
+    uri = URI("https://graph.instagram.com/me/media?fields=id,caption&access_token=#{ENV['USER_TOKEN']}")
+    resp = Net::HTTP.get_response(uri).body
+    resp = JSON.parse(resp)
+    @media = resp["data"][1]["id"] 
+  
+    uri = URI("https://graph.instagram.com/#{@media}/children?access_token=#{ENV['USER_TOKEN']}")
+    resp = Net::HTTP.get_response(uri).body
+    resp = JSON.parse(resp)
+    @array_item = resp["data"]
+
+    #location id　をもらってくる    エラー発生中
+    uri = URI("https://graph.instagram.com/#{@media}/fields=location?access_token=#{ENV['USER_TOKEN']}")
+    resp = Net::HTTP.get_response(uri).body
+    # resp = JSON.parse(resp)
+    @location = resp
+    
+    @mediaurl_v = []  #videoのurlが入る配列
+    @mediaurl_i = []  #imgのurlが入る配列
+    @array_item.each{|t|
+      uri = URI("https://graph.instagram.com/#{t["id"]}?fields=id,media_url&access_token=#{ENV['USER_TOKEN']}")
+      resp = Net::HTTP.get_response(uri).body
+      resp = JSON.parse(resp)
+      media_url = resp["media_url"]
+      if media_url[8] == "v"
+        @mediaurl_v.push(resp["media_url"])  
+      else
+        @mediaurl_i.push(resp["media_url"])
+      end
+    }
+  end
+
+>>>>>>> d83dc1a (map基礎)
 end
