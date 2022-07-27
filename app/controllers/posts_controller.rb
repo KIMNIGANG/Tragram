2c6801fe0a4ebd54299330bd87a7741198390131
pecokerochan <lisahirano@MacBook-Pro.local>
2022-07-28 00:44:54 +0900

    rebase

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -164,16 +164,8 @@ class PostsController < ApplicationController
     end
   end
 
-  def add_image()
-    post = Post.find(params[:id])
-    Cloudinary.config do |config|
-      config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
-      config.api_key = Rails.application.credentials.cloudinary[:api_key]
-      config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
-    end
-  end
-
   private
+
 
   def post?(id)
     post = Post.find_by(id: id)
